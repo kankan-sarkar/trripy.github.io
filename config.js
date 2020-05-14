aaef2a91152893ae1eae52d3b3dc77dc31247697
kankan <trripy01@gmail.com>
2020-05-14 15:38:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,5 +18,8 @@ var WebsiteData= {
         "Post2":{
             "Title":"Something Interesting","link":"document1.html","Author":"Morrison A","PublishDate":"20-07-2020","avatarimage":"images/avatar.jpg", "thumbnail":"images/pic01.jpg" ,"preface":"A knowledge base for Automobile Enthusiasts. This page is devoted to auto motive enthusiasts, who are turned on by the sound of the reviving engine or the smell of molten rubber on the track. Collect all the news around the word in the field of automobile, cars, bike, its' technology and the dynamics of vehicle from our experts in different fields of automobile industry."
         }
+        "Post3":{
+            "Title":"My First Post","link":"Something.html","Author":"Kankan","PublishDate":"20-07-2020","avatarimage":"images/avatar.jpg", "thumbnail":"images/pic01.jpg" ,"preface":"Written by kankan"
+        }
     }
 }
