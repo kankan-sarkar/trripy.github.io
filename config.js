bc206bffac6d59141a3c8fd39f820aacaa7cdd1f
Kankan <trripy01@gmail.com>
2020-05-14 15:46:06 +0100

    Hello2

--- config.js
+++ config.js
@@ -13,10 +13,55 @@ var WebsiteData= {
     },
     "MachineLearning":{
         "Post1":{
-            "Title":"Something Interesting","link":"document1.html","Author":"Morrison A","PublishDate":"20-07-2020","avatarimage":"images/avatar.jpg", "thumbnail":"images/pic01.jpg" ,"preface":"A knowledge base for Automobile Enthusiasts. This page is devoted to auto motive enthusiasts, who are turned on by the sound of the reviving engine or the smell of molten rubber on the track. Collect all the news around the word in the field of automobile, cars, bike, its' technology and the dynamics of vehicle from our experts in different fields of automobile industry."
+            "Title":"Something Interesting","link":"EKF.html","Author":"Morrison A","PublishDate":"20-07-2020","avatarimage":"images/avatar.jpg", "thumbnail":"images/pic01.jpg" ,"preface":"A knowledge base for Automobile Enthusiasts. This page is devoted to auto motive enthusiasts, who are turned on by the sound of the reviving engine or the smell of molten rubber on the track. Collect all the news around the word in the field of automobile, cars, bike, its' technology and the dynamics of vehicle from our experts in different fields of automobile industry."
         },
         "Post2":{
             "Title":"Something Interesting","link":"document1.html","Author":"Morrison A","PublishDate":"20-07-2020","avatarimage":"images/avatar.jpg", "thumbnail":"images/pic01.jpg" ,"preface":"A knowledge base for Automobile Enthusiasts. This page is devoted to auto motive enthusiasts, who are turned on by the sound of the reviving engine or the smell of molten rubber on the track. Collect all the news around the word in the field of automobile, cars, bike, its' technology and the dynamics of vehicle from our experts in different fields of automobile industry."
+        }
+    },
+    "CI/CD":{
+        "Post1":{
+            "Title":"Something Interesting","link":"EKF.html","Author":"Morrison A","PublishDate":"20-07-2020","avatarimage":"images/avatar.jpg", "thumbnail":"images/pic01.jpg" ,"preface":"A knowledge base for Automobile Enthusiasts. This page is devoted to auto motive enthusiasts, who are turned on by the sound of the reviving engine or the smell of molten rubber on the track. Collect all the news around the word in the field of automobile, cars, bike, its' technology and the dynamics of vehicle from our experts in different fields of automobile industry."
+        },
+        "Post2":{
+            "Title":"Something Interesting","link":"document1.html","Author":"Morrison A","PublishDate":"20-07-2020","avatarimage":"images/avatar.jpg", "thumbnail":"images/pic01.jpg" ,"preface":"A knowledge base for Automobile Enthusiasts. This page is devoted to auto motive enthusiasts, who are turned on by the sound of the reviving engine or the smell of molten rubber on the track. Collect all the news around the word in the field of automobile, cars, bike, its' technology and the dynamics of vehicle from our experts in different fields of automobile industry."
+        }
+    },
+    "Cards":{
+        "Card1":{
+            "Thumbnail":"images/pic04.jpg",
+            "link":"Test.html",
+            "Title":"Machine Learning",
+            "Date":"20-04-2020",
+            "AuthorIMG":"images/avatar.jpg",
+        },
+        "Card2":{
+            "Thumbnail":"images/pic05.jpg",
+            "link":"#",
+            "Title":"CICD",
+            "Date":"20-04-2020",
+            "AuthorIMG":"images/avatar.jpg",
+        },
+        "Card3":{
+            "Thumbnail":"images/pic06.jpg",
+            "link":"#",
+            "Title":"Data Science and Automotive",
+            "Date":"18-07-2020",
+            "AuthorIMG":"images/avatar.jpg",
+        },
+        "Card4":{
+            "Thumbnail":"images/pic07.jpg",
+            "link":"#",
+            "Title":"Autonomous Vehicles",
+            "Date":"19-09-2020",
+            "AuthorIMG":"images/avatar.jpg",
+        },
+        "Card5":{
+            "Thumbnail":"images/pic07.jpg",
+            "link":"#",
+            "Title":"Autonomous Vehicles",
+            "Date":"19-09-2020",
+            "AuthorIMG":"images/avatar.jpg",
         }
     }
 }
